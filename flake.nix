f3e671efd8562ca8903c80d8b54a104273988b65
Nico Bellack <nico@bellack.dev>
2024-08-22 11:45:03 +0200

    feat: add first files

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      
+      # Lenovo T14s
+      barahir = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+	modules = [
+          ./hosts/barahir/configuration.nix
+	  inputs.home-manager.nixosModules.default
+	];
+      };
+
+      # To add more configs:
+      # workmachine = nixpkgs.lib.nixosSystem { ... }
+
+    };
+  };
+}
---
+++ hosts/barahir/home.nix
@@ -0,0 +1,133 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../../modules/zsh.nix  
+  ];
+
+  home.username = "n2o";
+  home.homeDirectory = "/home/n2o";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+    keepassxc
+    nextcloud-client
+    vivaldi
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/n2o/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  
+  # Programs Settings
+  # =================
+  
+  # Git
+  programs.git = {
+    enable = true;
+    userName = "Nico Bellack";
+    userEmail = "nico@bellack.dev";
+  };
+
+  # Nextcloud
+  # Create a custom autostart entry for Nextcloud
+  home.file.".config/autostart/nextcloud.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Exec=${pkgs.nextcloud-client}/bin/nextcloud
+    Hidden=false
+    NoDisplay=false
+    X-GNOME-Autostart-enabled=true
+    Name=Nextcloud
+    Comment=Start Nextcloud client after login
+  '';
+
+  # Vim
+  programs.neovim = {
+    enable = true;
+  };
+
+  # VSCodium
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    userSettings = {};
+  };
+
+  ## Zsh
+  #programs.zsh = {
+  #  enable = true;
+  #  enableCompletion = true;
+  #  autosuggestion.enable = true;
+  #  syntaxHighlighting.enable = true;
+
+  #  shellAliases = {
+  #    vim = "nvim";
+  #  };
+    
+  #  oh-my-zsh = {
+  #    enable = true;
+  #    plugins = [ "git" ];
+  #    theme = "robbyrussell";
+  #  };
+  #};
+
+}
