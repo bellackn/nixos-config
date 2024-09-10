03c47f671474ffa13e8cb800a87500d8e7b1002c
Nico Bellack <nico@bellack.dev>
2024-09-10 22:47:34 +0200

    feat: make fingerprint scanner work

--- hosts/barahir/configuration.nix
+++ hosts/barahir/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, inputs, pkgs, ... }:
+{ config, inputs, lib, pkgs, ... }:
 
 {
   imports =
@@ -70,8 +70,6 @@
   # Enable touchpad support.
   services.libinput.enable = true;
 
-  services.gnome.gnome-keyring.enable = true;
-
   # Set default Shell
   users.defaultUserShell = pkgs.zsh;
   programs.zsh.enable = true;
@@ -107,6 +105,36 @@
     sops
     zlib
   ];
+
+  services.gnome.gnome-keyring.enable = true;
+
+  # Enable fingerprint scanning
+  services.fprintd = {
+    enable = true;
+  };
+
+  # Improve fingerprinting workflow (see https://github.com/NixOS/nixpkgs/issues/171136#issuecomment-1627779037)
+  security.pam.services.login.fprintAuth = false;
+  security.pam.services.gdm-fingerprint = lib.mkIf (config.services.fprintd.enable) {
+    text = ''
+      auth       required                    pam_shells.so
+      auth       requisite                   pam_nologin.so
+      auth       requisite                   pam_faillock.so      preauth
+      auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+      auth       optional                    pam_permit.so
+      auth       required                    pam_env.so
+      auth       [success=ok default=1]      ${pkgs.gdm}/lib/security/pam_gdm.so
+      auth       optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so
+
+      account    include                     login
+
+      password   required                    pam_deny.so
+
+      session    include                     login
+      session    optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+    '';
+  };
+
 
   # Install Docker.
   virtualisation.docker.enable = true;
