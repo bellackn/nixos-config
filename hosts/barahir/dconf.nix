2381234517cb90f17ec04c259609e64634f0a413
Nico Bellack <nico@bellack.dev>
2024-10-07 13:23:09 +0200

    chore: update dconf

--- hosts/barahir/dconf.nix
+++ hosts/barahir/dconf.nix
@@ -4,17 +4,26 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
-    # Input sources
-    "org/gnome/desktop/input-sources" =
-      {
-        mru-sources = [
-          (mkTuple [ "xkb" "us" ])
-        ];
-        sources = [
-          (mkTuple [ "xkb" "us" ])
-          (mkTuple [ "xkb" "de" ])
-        ];
-      };
+
+    "org/gnome/desktop/interface" = {
+      font-antialiasing = "rgba";
+      font-hinting = "slight";
+    };
+
+    "org/gnome/desktop/input-sources" = {
+      mru-sources = [
+        (mkTuple [ "xkb" "us" ])
+      ];
+      sources = [
+        (mkTuple [ "xkb" "us" ])
+        (mkTuple [ "xkb" "de" ])
+      ];
+    };
+
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = "appmenu:minimize,maximize,close";
+    };
+
 
     # Keyboard shortcuts
     "org/gnome/settings-daemon/plugins/media-keys" = {
@@ -23,11 +32,13 @@ with lib.hm.gvariant;
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
       ];
     };
+
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       name = "Alacritty w/ tmux";
       command = "alacritty -e tmux attach";
       binding = "<Super>Return";
     };
+
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
       name = "Alacritty";
       command = "alacritty";
