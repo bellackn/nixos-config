b48085dd2566b24f0a3c03d1e05a5c5eb55b9cc9
Nico Bellack <blcknc@pm.me>
2024-10-29 18:44:43 +0100

    feat: add nix-darwin

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   description = "NixOS config flake";
 
   inputs = {
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,7 +14,7 @@
     sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = { self, nixpkgs, nixvim, ... }@inputs: {
+  outputs = { self, nixpkgs, nixvim, darwin, ... }@inputs: {
     nixosConfigurations = {
 
       # Lenovo T14s
@@ -30,10 +32,24 @@
           inputs.sops-nix.nixosModules.sops
         ];
       };
-
-      # To add more configs:
-      # workmachine = nixpkgs.lib.nixosSystem { ... }
-
     };
+
+    darwinConfigurations = {
+
+      # MacBook Air M2
+      mair = darwin.lib.darwinSystem {
+        specialArgs = { inherit inputs; };
+        system = "aarch64-darwin";
+        modules = [
+          ./hosts/mair/configuration.nix
+          inputs.home-manager.darwinModules.home-manager
+          {
+            inputs.home-manager.useGlobalPkgs = true;
+            inputs.home-manager.useUserPackages = true;
+          }
+        ];
+      };
+    };
+
   };
 }
---
+++ hosts/mair/README.md
@@ -0,0 +1,7 @@
+# nix-darwin 
+
+Install nix on darwin via:
+
+```
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+```
