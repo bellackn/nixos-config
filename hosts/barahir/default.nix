0e02cdddafa6e5d0c2e809f3324c64eb33415705
Nico Bellack <nico@bellack.dev>
2025-03-27 00:13:32 +0100

    feat: install TeamViewer

--- hosts/barahir/default.nix
+++ hosts/barahir/default.nix
@@ -158,6 +158,9 @@ in
     localNetworkGameTransfers.openFirewall = true;
   };
 
+  # Install TeamViewer
+  services.teamviewer.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
