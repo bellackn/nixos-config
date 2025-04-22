74183da96c46c96cd898b71b2e2473d31b23228e
Nico Bellack <nico@bellack.dev>
2025-04-22 17:08:28 +0200

    feat: enable dnsmasq; split DNS between vino and local setup

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -6,8 +6,37 @@
 }:
 
 {
-  networking.networkmanager.enable = true;
   networking.hostName = "barahir";
+
+  networking.networkmanager = {
+    enable = true;
+    dns = "none"; # dnsmasq takes care of this
+  };
+
+  networking.search = [
+    "z.lan"
+    "fritz.box"
+  ];
+
+  services.dnsmasq = {
+    enable = true;
+    settings = {
+      conf-dir = "/var/lib/dnsmasq";
+      listen-address = [
+        "127.0.0.1"
+        "::1"
+      ];
+      server = [
+        "192.168.0.100" # Pi-hole
+        "9.9.9.9"
+        "149.112.112.112"
+        "2620:fe::fe"
+        "2620:fe::9"
+      ];
+    };
+  };
+
+  networking.resolvconf.useLocalResolver = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -41,11 +70,6 @@
     home = {
       autostart = false;
       address = [ "192.168.66.2/32" ];
-      dns = [
-        "192.168.0.100"
-        "z.lan"
-        "fritz.box"
-      ];
       mtu = 1400;
       privateKeyFile = config.sops.secrets."vpn/home-key".path;
       peers = [
@@ -68,7 +92,6 @@
     proton = {
       autostart = false;
       address = [ "10.2.0.2/32" ];
-      dns = [ "10.2.0.1" ];
       privateKeyFile = config.sops.secrets."vpn/proton-key".path;
       peers = [
         {
@@ -86,10 +109,6 @@
     vino = {
       autostart = false;
       address = [ "10.192.122.14/32" ];
-      dns = [
-        "192.168.188.1"
-        "fritz.box"
-      ];
       privateKeyFile = config.sops.secrets."vpn/vino-key".path;
       peers = [
         {
@@ -102,6 +121,14 @@
           publicKey = "aRCDI7DHb6+e/VVh2+NHswnYHQwTn0KJDBvRzueVqi4=";
         }
       ];
+      postUp = ''
+        echo "server=/fritz.box/192.168.188.1" > /var/lib/dnsmasq/vino-vpn.conf
+        systemctl restart dnsmasq
+      '';
+      postDown = ''
+        rm -f /var/lib/dnsmasq/vino-vpn.conf
+        systemctl restart dnsmasq
+      '';
     };
   };
 
