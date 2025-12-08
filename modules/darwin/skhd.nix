2b10c6f66b5d39cd0943b7cdfe51981e7b9528e4
Nico Bellack <n.bellack@hundt-consult.de>
2025-12-08 09:17:31 +0100

    feat: re-enable skhd
    Removed launch-agent manually because of popup spam

--- modules/darwin/skhd.nix
+++ modules/darwin/skhd.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  # services.skhd = {
-  #   enable = false;
-  #   skhdConfig = ''
-  #     # Alacritty w/ tmux
-  #     cmd - return : open -a Alacritty --args -e tmux attach
-  #   '';
-  # };
+  services.skhd = {
+    enable = false;
+    skhdConfig = ''
+      # Alacritty w/ tmux
+      cmd - return : open -a Alacritty --args -e tmux attach
+    '';
+  };
 }
